e740279756eb1b104ce424b82c28de6bf9cf9dea
Alex <admin@aleksandrslarionovs.com>
2024-02-27 18:38:55 +0000

    Add prototype image

--- react-native/components/Map.js
+++ react-native/components/Map.js
@@ -20,13 +20,70 @@ import customMapStyle from "../styles/custom-map.js";
 const windowWidth = Dimensions.get("window").width;
 const windowHeight = Dimensions.get("window").height;
 
+const getXY = ({ northEast, southWest }, width, height, lat, lon) => {
+  const north = toRadians(northEast.latitude);
+  const south = toRadians(southWest.latitude);
+  const east = toRadians(northEast.longitude);
+  const west = toRadians(southWest.longitude);
+  // Coordinates above are a subsection of Oakland, CA
+
+  let mapWidth = width; // Assign your map width here
+  let mapHeight = height; // Assign your map height here
+
+  function toRadians(deg) {
+    return (deg * Math.PI) / 180;
+  }
+
+  function mercatorY(lat) {
+    return Math.log(Math.tan(lat / 2 + Math.PI / 4));
+  }
+
+  function locationToPixel(lat, lon) {
+    lat = toRadians(lat);
+    lon = toRadians(lon);
+    const ymin = mercatorY(south);
+    const ymax = mercatorY(north);
+    const xFactor = mapWidth / (east - west);
+    const yFactor = mapHeight / (ymax - ymin);
+
+    let y = mercatorY(lat);
+    let x = (lon - west) * xFactor;
+    y = (ymax - y) * yFactor;
+    return [x, y];
+  }
+
+  return locationToPixel(lat, lon);
+};
 export const Map = ({ lon, lat }) => {
+  const [zoom, setZoom] = useState(
+    Math.round(Math.log(360 / 10.0421) / Math.LN2)
+  );
   const [overlayUrl, setOverlayUrl] = useState("");
   const mapRef = useRef(null);
   const [userLocation, setUserLocation] = useState(null);
   const [maps, setMaps] = useState([]);
+  const [coords, setCoords] = useState(null);
 
-  const [count, setCount] = useState(0);
+  const [boundingBox, setBoundingBox] = useState(null);
+  useEffect(() => {
+    const bound = async () => {
+      return await mapRef.current.getMapBoundaries();
+    };
+    if (mapRef.current) {
+      bound().then((boundaries) => {
+        console.log(boundaries);
+        setBoundingBox(boundaries);
+        ``;
+      });
+    }
+  }, [zoom]);
+
+  useEffect(() => {
+    if (boundingBox === null) return;
+
+    const res = getXY(boundingBox, windowWidth, windowHeight * 0.7, lat, lon);
+    setCoords(res);
+  }, [boundingBox]);
 
   useEffect(() => {
     const getWeatherMaps = async () => {
@@ -49,8 +106,8 @@ export const Map = ({ lon, lat }) => {
         {
           latitude,
           longitude,
-          latitudeDelta: 2.0922,
-          longitudeDelta: 2.0421,
+          latitudeDelta: 10.0922,
+          longitudeDelta: 10.0421,
         },
         1000 // Animation duration in milliseconds
       );
@@ -86,7 +143,7 @@ export const Map = ({ lon, lat }) => {
             opacity={0.2}
           /> */
   }
-  const [zoom, setZoom] = useState(7.663145542144775);
+
   const getZoom = async () => {
     let coords = await mapRef.current.getCamera();
 
@@ -96,11 +153,22 @@ export const Map = ({ lon, lat }) => {
   let xtile = 0;
   let ytile = 0;
 
+  //  {
+  //    maps.length > 0 && (
+  //      <UrlTile
+  //        urlTemplate={`https://tilecache.rainviewer.com/v2/radar/${maps[count].path}/512/{z}/{x}/{y}/2/1_1.png`}
+  //        tileSize={512}
+  //        opacity={0.2}
+  //      />
+  //    );
+  //  }
+  console.log(zoom);
+  console.log(coords);
   return (
     <View style={{ flex: 1, width: windowWidth, height: windowHeight * 0.7 }}>
       <MapView
-        onRegionChange={() => {
-          getZoom();
+        onRegionChange={async () => {
+          await getZoom();
         }}
         style={{ flex: 1, width: "100%", height: "100%" }}
         provider={PROVIDER_GOOGLE}
@@ -108,19 +176,19 @@ export const Map = ({ lon, lat }) => {
         initialRegion={{
           latitude: lon,
           longitude: lat,
-          latitudeDelta: 2.0922,
-          longitudeDelta: 2.0421,
+          latitudeDelta: 10.0922,
+          longitudeDelta: 10.0421,
         }}
         showsTraffic={false}
         showsBuildings={false}
         customMapStyle={customMapStyle}
         showsUserLocation={false} // Turn off default blue dot
       >
-        {maps.length > 0 && (
+        {coords && (
           <UrlTile
-            urlTemplate={`https://tilecache.rainviewer.com/v2/radar/${maps[count].path}/512/{z}/{x}/{y}/2/1_1.png`}
+            urlTemplate={`https://tile.openweathermap.org/map/precipitation_new/0/0/0.png?appid=0dc3ce6cf50014a8adc95cfd39ef6d2c`}
             tileSize={512}
-            opacity={0.2}
+            opacity={0.9}
           />
         )}
       </MapView>

3fd5d08a4cec6392ba3f5a4f489e2daaa10b2236
Alex <admin@aleksandrslarionovs.com>
2024-02-29 15:52:03 +0000

    Add workbox p1

--- pwa/webpack.config.js
+++ pwa/webpack.config.js
@@ -1,53 +1,56 @@
-const path = require("path");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const HtmlWebpackHarddiskPlugin = require("html-webpack-harddisk-plugin");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const webpack = require("webpack");
-const Dotenv = require("dotenv-webpack");
-
-process.env.NODE_ENV = process.env.NODE_ENV || "development";
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackHarddiskPlugin = require('html-webpack-harddisk-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
+const Dotenv = require('dotenv-webpack');
+const WorkboxWebpackPlugin = require('workbox-webpack-plugin');
+process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
 module.exports = (environment) => {
   const env = process.env.NODE_ENV.trim();
-  console.log("!!! Environment is " + env + " !!!");
+  console.log('!!! Environment is ' + env + ' !!!');
 
   return {
-    entry: ["./src/app.js"],
+    entry: ['./src/app.js'],
     mode: env,
     output: {
-      path: path.join(__dirname, "public", "dist"),
-      filename: "[name].[contenthash].js",
-      chunkFilename: "[name].[contenthash].js",
-      publicPath: "/dist/",
+      path: path.join(__dirname, 'public', 'dist'),
+      filename: '[name].[contenthash].js',
+      chunkFilename: '[name].[contenthash].js',
+      publicPath: '/dist/',
     },
     optimization: {
       emitOnErrors: false,
-      moduleIds: "deterministic",
-      runtimeChunk: "single",
+      moduleIds: 'deterministic',
+      runtimeChunk: 'single',
       splitChunks: {
         cacheGroups: {
           vendor: {
             test: /[\\/]node_modules[\\/]/,
-            name: "vendors",
-            chunks: "all",
+            name: 'vendors',
+            chunks: 'all',
           },
         },
       },
     },
     plugins: [
-      ...(env !== "production" ? [new HtmlWebpackHarddiskPlugin()] : []),
+      ...(env !== 'production' ? [new HtmlWebpackHarddiskPlugin()] : []),
       new HtmlWebpackPlugin({
-        filename: "../index.html",
-        template: "template.html",
+        filename: '../index.html',
+        template: 'template.html',
         alwaysWriteToDisk: true,
       }),
       new MiniCssExtractPlugin({
-        filename:
-          env === "production"
-            ? "dist/styles.[contenthash].css"
-            : "dist/styles.css",
+        filename: env === 'production' ? 'dist/styles.[contenthash].css' : 'dist/styles.css',
       }),
       new Dotenv(),
+      new WorkboxWebpackPlugin.GenerateSW({
+        swDest: './sw.js',
+        exclude: [/\.html$/],
+        clientsClaim: true,
+        skipWaiting: true,
+      }),
     ],
     module: {
       rules: [
@@ -56,42 +59,42 @@ module.exports = (environment) => {
           exclude: /node_modules/,
           use: [
             {
-              loader: "babel-loader",
+              loader: 'babel-loader',
             },
           ],
         },
         {
           test: /\.s?css$/,
           use: [
-            env === "production" ? MiniCssExtractPlugin.loader : "style-loader",
+            env === 'production' ? MiniCssExtractPlugin.loader : 'style-loader',
             {
-              loader: "css-loader",
-              options: { sourceMap: env === "development" },
+              loader: 'css-loader',
+              options: { sourceMap: env === 'development' },
             },
             {
-              loader: "sass-loader",
-              options: { sourceMap: env === "development" },
+              loader: 'sass-loader',
+              options: { sourceMap: env === 'development' },
             },
-            "postcss-loader",
+            'postcss-loader',
           ],
         },
       ],
     },
     resolve: {
-      extensions: [".ts", ".js", ".json", ".tsx"],
+      extensions: ['.ts', '.js', '.json', '.tsx'],
     },
-    devtool: env === "production" ? "source-map" : "inline-source-map",
+    devtool: env === 'production' ? 'source-map' : 'inline-source-map',
     devServer: {
-      allowedHosts: ["all"],
-      static: path.join(__dirname, "public/"),
+      allowedHosts: ['all'],
+      static: path.join(__dirname, 'public/'),
       historyApiFallback: true,
       devMiddleware: {
-        publicPath: "/dist/",
+        publicPath: '/dist/',
       },
-      host: "0.0.0.0",
+      host: '0.0.0.0',
       port: 8080,
       client: {
-        webSocketURL: "auto://0.0.0.0:0/ws", // note the `:0` after `0.0.0.0`
+        webSocketURL: 'auto://0.0.0.0:0/ws', // note the `:0` after `0.0.0.0`
       },
     },
   };
